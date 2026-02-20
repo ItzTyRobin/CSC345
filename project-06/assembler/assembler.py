5b5f13bbd172720784700117d9c263a2ff3dd638
ebishop898 <142810286+ebishop898@users.noreply.github.com>
2026-02-19 23:01:31 -0500

    started on assembler

--- project-06/assembler/assembler.py
+++ project-06/assembler/assembler.py
@@ -1,4 +1,17 @@
-## Takes an .asm filename
-## Prints cleaned instructions
+import os, sys
 
-import sys
+mainFile = sys.argv[1]
+
+# strip out the empty lines and comments 
+def clean_line(line): 
+    for line in mainFile : 
+        currentLine = cleanLine(line) ## removes all whitespace and comments from the line 
+
+def cleanLine(line): 
+    currentChar = line[0]
+    if currentChar == '/' or currentChar == '\n':
+        return ''
+    elif currentChar == ' ':
+        return cleanLine(line[1:])
+    else:
+        return line
