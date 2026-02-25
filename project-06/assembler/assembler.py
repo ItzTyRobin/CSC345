ba10e77835cbb542eda7eb67539988120c8a842a
ebishop898 <142810286+ebishop898@users.noreply.github.com>
2026-02-24 23:51:10 -0500

    I think i finished part 2, havent fully tested it

--- project-06/assembler/assembler.py
+++ project-06/assembler/assembler.py
@@ -1,117 +1,125 @@
 import sys
-global inputFile
-# global binaryLine
+
+nextVariableAddress = 16
 
 # To run:
 # cd project-06/assembler
 # python assembler.py ../tests/Add.asm
 # ^^ should add the Add.hack file to the /tests folder
 
+
 def main():
-   if len(sys.argv) > 1:
-       inputFile = sys.argv[1]
-   else:
-       inputFile = "../tests/Prog.asm"
+    if len(sys.argv) > 1:
+        inputFile = sys.argv[1]
+    else:
+        inputFile = "../tests/Prog.asm"
 
-   outputFile = inputFile.replace(".asm", ".hack")
+    outputFile = inputFile.replace(".asm", ".hack")
 
-   with open(inputFile, "r") as asm, open(outputFile, "w") as hack:
-       for line in asm:
-           # step 1: clean the file
-           # just removes any comments in the .asm file and then writes to the .hack file 
-           lines = cleanLine(line)
-           lines = decideAorC(lines) 
-           hack.write(lines)
-  
-   # read the file
-   print("The current input is in:", inputFile)
-   print("The current output is in:", outputFile)
+    with open(inputFile, "r") as asm:
+        # turn the OGasm file into a cleaned file without commenst or white spaces
+        cleaned = []
+        for line in asm:
+            newLine = cleanLine(line)
+            if newLine != "":
+                cleaned.append(newLine)
+
+    symbolTable = createSymbolTable(cleaned)  # pass 1:
+    binaryLines = createBinaryLines(cleaned, symbolTable)  # pass 2:
+
+    with open(outputFile, "w") as hack:
+        for line in binaryLines:
+            hack.write(line + "\n")
+
+    # read the file
+    print("The current input is in:", inputFile)
+    print("The current output is in:", outputFile)
+
 
 #    # step 2: create the symbol table
 #    symbolTable = createSymbolTable(lines)
-
 
 
 # -----------------------------
 # step 1: clean the file
 # -----------------------------
 def cleanLine(line):
-    # remove comments and in-line comments 
+    # remove comments and in-line comments
     line = line.split("//")[0]
 
-    # remove whitespace 
+    # remove whitespace
     line = line.strip()
 
-    #ignore empty lines 
+    # ignore empty lines
     if line == "":
         return ""
-    
-    return line + "\n"
 
-# def decideAorC(lines): 
-#     binaryLine = ""
-#     for line in lines:
-#         if line.startswith('@'):
-#          # this is an A-instruction
-#             binaryLine = createAInstruction(line, )
-#         else:
-#         # this is a C-instruction
-#             binaryLine = createCInstruction(line)
-#     return binaryLine
+    return line
 
-def decideAorC(line):
-    line = line.strip() 
+
+def decideAorC(line, symbolTable):
+    line = line.strip()
     if line == "":
+        return ""
+
+    if line.startswith("(") and line.endswith(")"):
         return ""
 
     if line.startswith("@"):
-        return createAInstruction(line) + "\n"
+        return createAInstruction(line, symbolTable)
     else:
-        return createCInstruction(line) + "\n"
+        return createCInstruction(line)
 
-def createAInstruction(line):
-    number = line[1:] 
-    if not number.isdigit():
-        raise ValueError(f"Milestone 1 only supports numeric A-instructions, got: {line}")
 
-    value = int(number)
-    if value < 0 or value > 32767:
-        raise ValueError(f"A-instruction out of range (0..32767): {line}")
+def createAInstruction(line, symbolTable):
+    global nextVariableAddress
+    number = line[1:]
 
-    return "0" + format(value, "015b")
+    # @number
+    if number.isdigit():
+        value = int(number)
+        return "0" + format(value, "015b")
+
+    # @symbol
+    if number not in symbolTable:
+        symbolTable[number] = nextVariableAddress
+        nextVariableAddress += 1
+
+    return "0" + format(symbolTable[number], "015b")
+
 
 dest_table = {
-    "":    "000",
-    "M":   "001",
-    "D":   "010",
-    "MD":  "011",
-    "A":   "100",
-    "AM":  "101",
-    "AD":  "110",
+    "": "000",
+    "M": "001",
+    "D": "010",
+    "MD": "011",
+    "A": "100",
+    "AM": "101",
+    "AD": "110",
     "AMD": "111",
 }
 
 jump_table = {
-    "":     "000",
-    "JGT":  "001",
-    "JEQ":  "010",
-    "JGE":  "011",
-    "JLT":  "100",
-    "JNE":  "101",
-    "JLE":  "110",
-    "JMP":  "111",
+    "": "000",
+    "JGT": "001",
+    "JEQ": "010",
+    "JGE": "011",
+    "JLT": "100",
+    "JNE": "101",
+    "JLE": "110",
+    "JMP": "111",
 }
 
 comp_table = {
-    "0":   "0101010",
-    "1":   "0111111",
-    "-1":  "0111010",
-    "D":   "0001100",
-    "A":   "0110000",
-    "!D":  "0001101",
-    "!A":  "0110001",
-    "-D":  "0001111",
-    "-A":  "0110011",
+    "0": "0101010",
+    "1": "0111111",
+    "-1": "0111010",
+    "D": "0001100",
+    "A": "0110000",
+    "!D": "0001101",
+    "!A": "0110001",
+    "-D": "0001111",
+    "-A": "0110011",
     "D+1": "0011111",
     "A+1": "0110111",
     "D-1": "0001110",
@@ -121,9 +129,9 @@ comp_table = {
     "A-D": "0000111",
     "D&A": "0000000",
     "D|A": "0010101",
-    "M":   "1110000",
-    "!M":  "1110001",
-    "-M":  "1110011",
+    "M": "1110000",
+    "!M": "1110001",
+    "-M": "1110011",
     "M+1": "1110111",
     "M-1": "1110010",
     "D+M": "1000010",
@@ -134,24 +142,13 @@ comp_table = {
 }
 
 
-
-#    # step 3: convert the assembly code to binary machine code 
+#    # step 3: convert the assembly code to binary machine code
 #    binaryLines = createBinaryLines(lines, symbolTable)
 
 #    # step 4: write the binary machine code to a .hack file
 #    writeToHackFile(binaryLines, outputFile)
 
-# -----------------------------
-# step 2: create the symbol table
-# -----------------------------
-def createSymbolTable(lines):
-   """
-   we want to create a symbol table that maps the
-   symbols in the assembly code to their corresponding
-   addresses in the machine code.
-   """
-    
-  
+
 def createCInstruction(line):
     destSymbol = ""
     compSymbol = ""
@@ -167,7 +164,7 @@ def createCInstruction(line):
     else:
         compSymbol = rest
         jumpSymbol = ""
-        
+
     destSymbol = destSymbol.strip()
     compSymbol = compSymbol.strip()
     jumpSymbol = jumpSymbol.strip()
@@ -179,6 +176,57 @@ def createCInstruction(line):
     # build full C-instruction
     binary = "111" + comp_bits + dest_bits + jump_bits
     return binary
+
+
+table = {
+    "R0": 0,
+    "R1": 1,
+    "R2": 2,
+    "R3": 3,
+    "R4": 4,
+    "R5": 5,
+    "R6": 6,
+    "R7": 7,
+    "R8": 8,
+    "R9": 9,
+    "R10": 10,
+    "R11": 11,
+    "R12": 12,
+    "R13": 13,
+    "R14": 14,
+    "R15": 15,
+    "SCREEN": 16384,
+    "KBD": 24576,
+}
+
+
+def createSymbolTable(lines):
+    """
+    we want to create a symbol table that maps the
+    symbols in the assembly code to their corresponding
+    addresses in the machine code.
+    """
+    romAddress = 0
+    for line in lines:
+        if line.startswith("(") and line.endswith(")"):
+            symbol = line[1:-1]
+            if symbol not in table:
+                table[symbol] = romAddress
+        else:
+            romAddress += 1
+
+    return table
+
+
+def createBinaryLines(lines, symbolTable):
+    binaryLines = []
+
+    for line in lines:
+        binary = decideAorC(line, symbolTable)
+        if binary != "":
+            binaryLines.append(binary)
+
+    return binaryLines
 
 
 main()
