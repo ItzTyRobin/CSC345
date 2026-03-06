3f30da2d6cfbe85217712317a8c44ef6aafc16be
ebishop898 <142810286+ebishop898@users.noreply.github.com>
2026-03-06 18:26:37 -0500

    done with project 7

--- project-07/VMTranslator.py
+++ project-07/VMTranslator.py
@@ -103,11 +103,7 @@ def translateMemoryAccess(command, segment, index, filename):
     return asm
 
 
-def translateLabel(label_name):
-    return [f"({label_name})"]
-
-
-def translateArithmetic(command, counter=0):
+def translateArithmetic(command, counter):
     asm = []
     
     if command == "add":
@@ -142,6 +138,84 @@ def translateArithmetic(command, counter=0):
         asm.append("@SP")
         asm.append("M=M+1")
         
+    elif command == "eq": 
+        asm.append("@SP")
+        asm.append("M=M-1")
+        asm.append("A=M")
+        asm.append("D=M")
+        asm.append("@SP")
+        asm.append("M=M-1")
+        asm.append("A=M")
+        asm.append("D=M-D")
+        asm.append(f"@EQ_TRUE{counter}")
+        asm.append("D;JEQ")
+        asm.append("@SP")
+        asm.append("A=M")
+        asm.append("M=0")
+        asm.append(f"@EQ_END{counter}")
+        asm.append("0;JMP")
+        asm.append(f"(EQ_TRUE{counter})")
+        asm.append("@SP")
+        asm.append("A=M")
+        asm.append("M=-1")
+        asm.append(f"(EQ_END{counter})")
+        
+    elif command == "gt": 
+        asm.append("@SP")
+        asm.append("M=M-1")
+        asm.append("A=M")
+        asm.append("D=M")
+        asm.append("@SP")
+        asm.append("M=M-1")
+        asm.append("A=M")
+        asm.append("D=M-D")
+        asm.append(f"@GT_TRUE{counter}")
+        asm.append("D;JGT")
+        asm.append("@SP")
+        asm.append("A=M")
+        asm.append("M=0")
+        asm.append(f"@GT_END{counter}")
+        asm.append("0;JMP")
+        asm.append(f"(GT_TRUE{counter})")
+        asm.append("@SP")
+        asm.append("A=M")
+        asm.append("M=-1")
+        asm.append(f"(GT_END{counter})")
+    
+    elif command == "lt":
+        asm.append("@SP")
+        asm.append("M=M-1")
+        asm.append("A=M")
+        asm.append("D=M")
+        asm.append("@SP")
+        asm.append("M=M-1")
+        asm.append("A=M")
+        asm.append("D=M-D")
+        asm.append(f"@LT_TRUE{counter}")
+        asm.append("D;JLT")
+        asm.append("@SP")
+        asm.append("A=M")
+        asm.append("M=0")
+        asm.append(f"@LT_END{counter}")
+        asm.append("0;JMP")
+        asm.append(f"(LT_TRUE{counter})")
+        asm.append("@SP")
+        asm.append("A=M")
+        asm.append("M=-1")
+        asm.append(f"(LT_END{counter})")
+
+    elif command == "and":
+        asm.append("@SP")
+        asm.append("M=M-1")
+        asm.append("A=M")
+        asm.append("D=M")
+        asm.append("@SP")
+        asm.append("M=M-1")
+        asm.append("A=M")
+        asm.append("M=D&M")
+        asm.append("@SP")
+        asm.append("M=M+1")
+        
     elif command == "or":
         asm.append("@SP")
         asm.append("M=M-1")
@@ -153,8 +227,26 @@ def translateArithmetic(command, counter=0):
         asm.append("M=D|M")
         asm.append("@SP")
         asm.append("M=M+1")
-
+        
+    elif command == "not": 
+        asm.append("@SP")
+        asm.append("M=M-1")
+        asm.append("A=M")
+        asm.append("M=!M")
+        asm.append("@SP")
+        asm.append("M=M+1")
+    
     return asm
+
+def cleanLine(line):
+    line = line.split("//")[0]
+
+    line = line.strip()
+
+    if line == "":
+        return ""
+
+    return line
 
 
 def main(): 
@@ -165,12 +257,44 @@ def main():
         
     outputFile = inputFile.replace(".vm", ".asm")
     
+    fileNameSplit = inputFile.split("/")
+    fileNameforTranslate = fileNameSplit[-1].replace(".vm", "")
+    
     print("Input file:", inputFile)
     print("Output file:", outputFile)
     
     with open(inputFile, "r") as vm:
-        lines = vm.readlines()
+        cleaned = []
+        for line in vm:
+            newLine = cleanLine(line)
+            if newLine != "":
+                cleaned.append(newLine)
+                
+    asmLines = []
+    counter = 0 # need this for the eq, gt, lt commands to create unique labels
+    
+    for line in cleaned:
+        wordsInLine = line.split() 
         
+        if wordsInLine[0] == "push" or wordsInLine[0] == "pop":
+            command = wordsInLine[0]
+            segment = wordsInLine[1]
+            index = int(wordsInLine[2])
+            
+            translated = translateMemoryAccess(command, segment, index, fileNameforTranslate)
+            asmLines.extend(translated)
+            
+        else: 
+            command = wordsInLine[0]
+            translated = translateArithmetic(command, counter)
+            asmLines.extend(translated)
+             
+            if command in ["eq", "gt", "lt"]:
+                counter += 1
+                
     with open(outputFile, "w") as asm:
-        for line in lines:
-            asm.write(line)
+        # for line in asmLines:
+        #     asm.write(line + "\n")
+        asm.write("\n".join(asmLines))
+            
+main()   
